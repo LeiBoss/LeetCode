c659a7f40e2cae695f310136d80cc641f8a56723
LeiYW <1902862824@qq.com>
2018-12-23 16:14:38 +0800

    '忽略文件设置'

---
+++ LeetCode/My/LeetCode20181223.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.My
+{
+    public class LeetCode20181223
+    {
+        public static bool IsValid(string s)
+        {
+            bool isValid = true;
+            if (!string.IsNullOrEmpty(s))
+            {
+
+            }
+            return isValid;
+        }
+    }
+}
